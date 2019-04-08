3a990128a00786769242bfc4b6ba7271779d051a
iKnowJavaScript <remymartins0398@yahoo.com>
2019-04-08 17:32:50 +0100

    Write test for Add operation

---
+++ operations/operations.test.js
@@ -0,0 +1,14 @@
+const { add } = require("./operations");
+
+//Testing Add implementation
+describe("Testing Add implementation", () => {
+  it("Add of 1 and 1 equals 2", () => {
+    expect(add(1, 1)).toBe(2);
+  });
+  it("Add of Two large sum", () => {
+    expect(add(233, 152)).toBe(385);
+  });
+  it("Parameter should only be Numbers", () => {
+    expect(add(4, "4")).toBe("Input Must be Numbers only");
+  });
+});
