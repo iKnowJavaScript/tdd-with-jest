b6c4f8d844a6417921a34fdf7c4de83aa7f52036
iKnowJavaScript <remymartins0398@yahoo.com>
2019-04-09 09:41:52 +0100

    Add codes implementation for string concatenation operation

--- operations/operations.js
+++ operations/operations.js
@@ -43,7 +43,10 @@ const divide = function (a, b) {
   return typeof a === "number" && typeof b === "number" ? a / b : false;
 }
 
-
+//String Concatenation Operation
+const joinString = function(a, b) {
+  return typeof a === "string" && typeof b === "string" ? a.concat(b) : false;
+}
 
 module.exports = { add, addAll, subtract, multiply, divide, joinString };
 
