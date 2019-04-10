ce5f6a7989817d2a9411b0916d7053946af6f43d
iKnowJavaScript <remymartins0398@yahoo.com>
2019-04-10 12:06:57 +0100

    All files up-to-date

---
+++ operations/git_api/tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+const multiply = function() {
+  
+  if(arguments.length < 2) return "Input at least two Number";
+
+  let total = 1;
+  for (let arg in arguments) {
+    let loop = typeof arguments[arg] === "number";
+
+    if (!loop) {
+      throw new Error("Only Numbers are allowed");
+    } else {
+      total *= arguments[arg];
+    }
+  }
+  return total;
+};
+
+console.log(multiply());
