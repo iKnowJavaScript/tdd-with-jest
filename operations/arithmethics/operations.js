92e13e06a19ef81aa69c8e13af0c0ad09c211d1f
iKnowJavaScript <remymartins0398@yahoo.com>
2019-04-10 11:30:21 +0100

    All test cases for two sum pass

--- operations/arithmethics/operations.js
+++ operations/arithmethics/operations.js
@@ -1,6 +1,8 @@
 //Addition Operation
 const twoSum = function(a, b) {
-  return typeof a === "number" && typeof b === "number"
+  return arguments.length < 2 || arguments.length > 3
+  ? "Input only two Numbers"
+  : typeof a === "number" && typeof b === "number"
     ? a + b
     : "Inputs Must be Numbers";
 };
