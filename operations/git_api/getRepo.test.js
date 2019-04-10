2e0ad84203516541e5acb91e90649d05e3ab1229
iKnowJavaScript <remymartins0398@yahoo.com>
2019-04-10 12:17:06 +0100

    Add some test cases for getRepo implementetion

--- operations/git_api/getRepo.test.js
+++ operations/git_api/getRepo.test.js
@@ -1,1 +1,38 @@
+let showGitHubRepos = require("./getRepo")
+
+describe("Mock testing showGitHubRepos implementation", () => {
+  it("Test user with repositories", async () => {
+    showGitHubRepos = jest
+      .fn()
+      .mockResolvedValue([
+        "Ball-Bouncing-and-Collision",
+        "css-grid",
+        "CSS-Grid-Calculator",
+        "CSS-Grid-Phone",
+        "css-protips",
+        "Databaseless-API"
+      ]);
+
+    let repos = await showGitHubRepos().then(data => data);
+
+    expect(showGitHubRepos).toHaveBeenCalled();
+    expect(repos).toContain("CSS-Grid-Phone");
+  });
+  it("Should return 'User has not repository yet' if it return an empty array", async () => {
+    showGitHubRepos = jest.fn().mockResolvedValue([]);
+
+    let repos = await showGitHubRepos().then(data => data);
+
+    expect(showGitHubRepos).toHaveBeenCalled();
+    expect(repos).toEqual([]);
+  });
+  // it("Should return 'User not found' if no array was returned", async () => {
+  //   showGitHubRepos = jest.fn().mockRejectedValue(new Error("User Not Found"));
+
+  //   const error = await showGitHubRepos();
+
+  //   expect(showGitHubRepos).toHaveBeenCalled();
+  //   expect(error).toMatch(/Not Found/);
+  // });
+});
 
