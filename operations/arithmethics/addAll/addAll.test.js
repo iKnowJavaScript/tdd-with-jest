807bdc502dcbbdf9862eab23035e8d2141f46230
iKnowJavaScript <remymartins0398@yahoo.com>
2019-04-12 16:26:47 +0100

    Manipulate dome test cases to be sure of the flexibility

--- operations/arithmethics/addAll/addAll.test.js
+++ operations/arithmethics/addAll/addAll.test.js
@@ -1,4 +1,4 @@
-const addAll = require('./addAll');
+const addAll = require("./addAll");
 
 describe("Testing add operation for Infinite numbers", () => {
   it("Addition of 1 and 1 equals 2", () => {
@@ -8,7 +8,7 @@ describe("Testing add operation for Infinite numbers", () => {
     expect(addAll(11, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10)).toBe(73);
   });
   it("Parameter should at least be two", () => {
-    expect(addAll(1)).toMatch(/least two Number/);
+    expect(addAll()).toMatch(/least two Number/);
   });
   it("Parameter should only be Numbers", () => {
     function logError() {
